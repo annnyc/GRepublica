3249d94ec4421e6e56beff8856933b2fec6c04a4
mazarafa <arruda.rafa.ra@gmail.com>
2020-11-26 23:07:25 -0400

    adicionando about

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,65 +1,70 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>G-REPUBLICA</title>
+    <title>G-REPUBLICA</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
-        
-    </head>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
 
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="navBar">
+</head>
 
-                @if (Route::has('login'))
-                <div class="top-right links">
-                   
-                    @auth
-                    <a href="{{ url('/') }}">Home</a>
-                    @else
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="navBar">
 
-                    <a href="{{ route('login') }}">Login</a>
+            @if (Route::has('login'))
+            <div class="top-right links">
 
-                    @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
-                    @endif
-                    @endauth
-                </div>
+                @auth
+                <a href="{{ url('/') }}">Home</a>
+                @else
+
+                <a href="{{ route('login') }}">Login</a>
+
+                @if (Route::has('register'))
+                <a href="{{ route('register') }}">Register</a>
                 @endif
+                @endauth
             </div>
-
-            <div class="content">
-
-                <!-- <img src="{!! asset('img/chicago.jpg') !!}"> -->
-                <div class="title m-b-md">
-                    G REPÚBLICAS
-                </div>
-
-
-            </div>
+            @endif
         </div>
-    </body>
+
+        <div class="content">
+
+            <!-- <img src="{!! asset('img/chicago.jpg') !!}"> -->
+            <div class="title m-b-md">
+                G REPÚBLICAS
+            </div>
+
+
+        </div>
+    </div>
+
     <footer>
-        
+
         <div class="slow">
-            <p><a href="#">Quem Somos</a></p>
-            <p><a href="#">Atendimento</a></p>
-            <p><a href="#">FAQ</a></p>
+
+            <p>
+                <a href="{{ route('about') }}">
+                    {{ __('Quem Somos') }}
+                </a>
+            </p>
+            <p><a>Atendimento</a></p>
+            <p><a>FAQ</a></p>
         </div>
 
         <div class="down">
-            <p><a href="#">Suporte</a></p>
-            <p><a href="#">G- Republica PRO</a></p>
-            <p><a href="#">Central de Ajuda</a></p>
+            <p><a>Parceirias</a></p>
+            <p><a>G- Republica PRO</a></p>
+            <p><a>Central de Ajuda</a></p>
         </div>
         <br>
-        <p>©Copyright Mazarafa 2020</p>
+        <p>©Copyright G-Republica 2021</p>
     </footer>
-
+</body>
 </html>
